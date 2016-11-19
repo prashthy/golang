2186ce22d144eac7146a3a7ec36106f5e35da25a
prashant <prashthy@gmail.com>
2016-11-18 19:41:20 -0800

    Initial revision of SpanEng

---
+++ SpanEng/spanish_english.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	//"io"
+	"io/ioutil"
+	//"os"
+	"strings"
+	"math/rand"
+	"time"
+)
+
+var filename string = "dict.txt"
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	content, err := ioutil.ReadFile(filename)
+	check(err)
+	lines := strings.Split(string(content), "\n")
+
+	var spanWords = make([]string, len(lines))
+	var engWords = make([]string, len(lines))
+
+	//spanToEng = make(map[string]string)
+	//engToSpan = make(map[string]string)
+
+	for i, val := range lines {
+		tmp := strings.Split(val, "=")
+		if tmp[0] != "" {
+			spanWords[i] = tmp[0]
+			engWords[i] = tmp[1]
+		}
+		i++
+	}
+	fmt.Println("spanWords")
+	for _, value := range spanWords {
+		fmt.Println(value);
+	}
+	fmt.Println("engWords")
+	for _, value := range engWords {
+		fmt.Println(value);
+	}
+	numWords := len(spanWords)
+	rand.Seed(time.Now().UnixNano())
+	for {
+		idx := rand.Intn(numWords)
+		fmt.Println(engWords[idx])
+		var i byte
+		fmt.Scanf("Press any key to continue: %c", &i)
+		fmt.Println(spanWords[idx])
+		fmt.Println()
+		fmt.Scanf("Press any key to continue: %c", &i)
+	}
+}
